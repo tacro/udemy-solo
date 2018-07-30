953801073c8d1579440d77d137d009aeb84c7d71
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-30 13:59:10 +0900

    added owl carousel

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', function(){
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 });
+
+/* =====================================
+  Team
+======================================*/
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartspeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    dots: false,
+  });
+});
