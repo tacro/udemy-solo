023c85dbfeb47dcb50613ded0f296a783e63703a
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-30 19:41:56 +0900

    animated progress bars

--- js/script.js
+++ js/script.js
@@ -21,3 +21,22 @@ $(function(){
     dots: false,
   });
 });
+
+/* =====================================
+  Progress Bars
+======================================*/
+$(function(){
+
+  $("#progress-elements").waypoint(function(direction){
+    // alert("you reached to me");
+    $(".progress-bar").each(function(){
+      $(this).animate({
+        width: $(this).attr("aria-valuenow") + "%",
+      },1000);
+    })
+    this.destroy();
+  }, {
+    offset: 'bottom-in-view'
+  });
+
+});
