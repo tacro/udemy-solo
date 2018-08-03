10c842acbb59040f52c3617dfe8c669609a9805b
Tacro Hashimoto <tacro@g.ucla.edu>
2018-08-03 15:11:50 +0900

    small fixes

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $(function() {
     $(".progress-bar").each(function() {
       $(this).animate({
         width: $(this).attr("aria-valuenow") + "%",
-      }, 1000);
+      }, 1500);
     })
     this.destroy();
   }, {
