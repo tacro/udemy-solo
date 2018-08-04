50881e7ffee22032924b92005214ff8eb7055f85
Tacro Hashimoto <tacro@g.ucla.edu>
2018-08-04 19:50:55 +0900

    google map is working

--- js/script.js
+++ js/script.js
@@ -203,7 +203,7 @@ $(function() {
 
 $(window).on('load', function() {
 
-  var addressString = "brooklyn, NY";
+  var addressString = "Brooklyn, NY";
   var myLatLng = {
     lat: 40.678178,
     lng: -73.944158
@@ -213,6 +213,30 @@ $(window).on('load', function() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     center: myLatLng
+  });
+
+  // add marker
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Click to see address",
+  });
+
+  // add info window
+  var infoWindow = new google.maps.InfoWindow({
+    content: addressString
+  });
+
+  // show info window when user clicks marker
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+
+  // resize
+  google.maps.event.addDomListener(window, 'resize', function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
   });
 });
 
